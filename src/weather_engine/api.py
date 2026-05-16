2fa514ccc8cdffa6e1d0d8b3d8c1eefeb00124f4
jadosh <jadosh2000@gmail.com>
2026-05-16 15:38:24 +0300

    Adds a dummy return to test jinja. Now has jinja templates and serves html to the browser directly from backend and uses htmx to render

--- src/weather_engine/api.py
+++ src/weather_engine/api.py
@@ -1,30 +1,24 @@
-from fastapi import FastAPI
+from pathlib import Path
+from fastapi import FastAPI, Request
+from fastapi.responses import HTMLResponse
+from fastapi.templating import Jinja2Templates
 from weather_engine.database import SessionLocal
 from sqlalchemy import text
 
-
-class CellForecast():
-    cell_id: int
-    lat: float
-    lon: float
-    precipitation_t1: float
-    precipitation_t3: float
-    precipitation_t6: float
-    precipitation_t12: float
-
-
 app = FastAPI()
+templates = Jinja2Templates(directory=Path(__file__).parent / "templates")
 
-@app.get("/predict")
-def predict():
+@app.get("/", response_class=HTMLResponse)
+def index(request: Request):
+    return templates.TemplateResponse(request=request, name="index.html")
+
+@app.get("/predict", response_class=HTMLResponse)
+def predict(request: Request):
     query = """
     SELECT cf.*, cn.lat, cn.lon
     FROM cell_forecasts cf
     JOIN cell_neighbors cn ON cf.cell_id = cn.cell_id
     """
-    
     with SessionLocal() as db:
-        rows = db.execute(text(query)).fetchall()
-        return [dict(row._mapping) for row in rows]
-    
-    
+        rows = [dict(row._mapping) for row in db.execute(text(query)).fetchall()]
+    return templates.TemplateResponse(request=request, name="predict.html", context={"rows": rows})
